a473d53391dc7d29ff330545a19e784d8d2fa472
Grant McConnaughey <grantmcconnaughey@gmail.com>
2015-01-27 16:22:34 -0600

    Add docstrings to script functions

--- missing_docstrings.py
+++ missing_docstrings.py
@@ -18,7 +18,31 @@ FILES_TO_IGNORE = (
 )
 
 
-def file_to_ignore(file_name):
+def file_to_process(file_path):
+    """
+    Determines if a file should be processed by the script.
+
+    Args:
+        file_path (str): The path to the file.
+
+    Returns:
+        bool: True if the file should be processed by the script.
+    """
+    file_name = os.path.basename(file_path)
+    return file_name.endswith('.py')
+
+
+def file_to_ignore(file_path):
+    """
+    Determines if a file should be ignored by the script.
+
+    Args:
+        file_path (str): The path to the file.
+
+    Returns:
+        bool: True if the file should be ignored by the script.
+    """
+    file_name = os.path.basename(file_path)
     if file_name in FILES_TO_IGNORE:
         return True
     else:
@@ -26,34 +50,65 @@ def file_to_ignore(file_name):
 
 
 def is_function(line):
-    clean_line = line.strip()
-    return clean_line.startswith('def ')
+    """
+    Determines if a line in a file is a function declaration.
+
+    Args:
+        line (str): The line to check for a function declaration.
+
+    Returns:
+        bool: True if the line is a function declaration.
+    """
+    return line.strip().startswith('def ')
 
 
 def has_docstring(line):
-    clean_line = line.strip()
-    return clean_line.startswith('"""')
+    """
+    Determines if a line in a file is a docstring.
+
+    Args:
+        line (str): The line to check for a docstring.
+
+    Returns:
+        bool: True if the line is a docstring.
+    """
+    return line.strip().startswith('"""')
 
 
-def add_to_undocumented_functions(file, line):
-    if UNDOCUMENTED_FUNCTIONS.get(file, False):
-        UNDOCUMENTED_FUNCTIONS[file].append(line)
+def add_to_undocumented_functions(file_path, line):
+    """
+    Adds an undocumented function to the UNDOCUMENTED_FUNCTIONS constant.
+
+    Args:
+        file_path (str): The path to the file with the undocumented function.
+        line (str): The line that contains the undocumented function.
+    """
+    if UNDOCUMENTED_FUNCTIONS.get(file_path, False):
+        UNDOCUMENTED_FUNCTIONS[file_path].append(line)
     else:
-        UNDOCUMENTED_FUNCTIONS[file] = [line]
+        UNDOCUMENTED_FUNCTIONS[file_path] = [line]
 
 
-def process_python_file(file):
-    with open(file, 'r') as f:
+def process_file(file_path):
+    """
+    Scans a file line by line and finds each function that
+    does not have an associated docstring.
+
+    Args:
+        file_path (str): The path to the file.
+    """
+    with open(file_path, 'r') as f:
         lines = f.readlines()
         for i, line in enumerate(lines):
             if is_function(line):
                 if has_docstring(lines[i+1]):
                     DOCUMENTED_FUNCTIONS.append(line)
                 else:
-                    add_to_undocumented_functions(file, line)
+                    add_to_undocumented_functions(file_path, line)
 
 
 def print_results():
+    """Prints the results of the script's run."""
     num_undocumented_functions = 0
     # Print detail lines
     for file, lines in UNDOCUMENTED_FUNCTIONS.items():
@@ -68,6 +123,7 @@ def print_results():
     num_functions = num_documented_functions + num_undocumented_functions
     percent_documented = num_documented_functions / num_functions * 100
     print '{} files scanned.'.format(len(UNDOCUMENTED_FUNCTIONS.keys()))
+    print '{} total functions'.format(num_functions)
     print '{} documented functions, {} undocumented functions.'.format(
         num_documented_functions,
         num_undocumented_functions
@@ -76,13 +132,14 @@ def print_results():
 
 
 def main():
+    """Runs the script."""
     starting_directory = sys.argv[1]
     full_directory_path = os.path.abspath(starting_directory)
     for path, dirs, files in os.walk(full_directory_path):
         for f in files:
-            if f.endswith('.py') and not file_to_ignore(f):
-                full_file_path = os.path.join(path, f)
-                process_python_file(full_file_path)
+            file_path = os.path.join(path, f)
+            if file_to_process(file_path) and not file_to_ignore(file_path):
+                process_file(file_path)
     print_results()
 
 
